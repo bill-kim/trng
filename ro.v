ec414af997682af029251ab861c0247204ed5834
Bill Kim <munsungk@andrew.cmu.edu>
2019-04-30 17:12:33 -0400

    Fixed syntax

--- ro.v
+++ ro.v
@@ -1,3 +1,5 @@
+`default_nettype none
+
 module ro #(SIZE = 3, DELAY = 2) (
   output wire signal);
 
--- ro_comb.v
+++ ro_comb.v
@@ -1,22 +1,24 @@
+`default_nettype none
 `include "params.vh"
 
 module ro_comb (
   output wire output_comb);
 
-	wire singals[`NUM_OF_RO-1:0];
+	wire signals[`NUM_OF_RO-1:0];
 	wire xors[`NUM_OF_RO-2:0];
 	
 	genvar i;
 	generate
 		for (i = 0; i < `NUM_OF_RO; i = i + 1) begin: ring_oscillators
-			ro #(`NUM_OF_GATES, `GATE_DELAY) ring (singals[i]);
+			ro #(`NUM_OF_GATES, `GATE_DELAY) ring (signals[i]);
 		end
 
 		xor (xors[0], signals[0], signals[1]);
-		for (i = 0; i < `NUM_OF_RO-2; i++) begin: xor_gates
+		for (i = 0; i < `NUM_OF_RO-2; i = i + 1) begin: xor_gates
 			xor (xors[i+1], xors[i], signals[i+1]);
 		end
 	endgenerate
-  
+
+  assign output_comb = xors[`NUM_OF_RO-2];  
 
 endmodule

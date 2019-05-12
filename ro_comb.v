4e765e6fb00b7c87ecc91b9119c01d3e960778c7
Bill Kim <munsungk@andrew.cmu.edu>
2019-05-12 15:36:54 -0400

    fixed array size error

--- ro_comb.v
+++ ro_comb.v
@@ -7,7 +7,7 @@ module ro_comb (
   output wire output_comb);
 
 	wire signals[`NUM_OF_RO-1:0];
-	reg [`NUM_OF_RO-1:0] xors[$clog2(`NUM_OF_RO):0];
+	reg [`NUM_OF_RO:0] xors[$clog2(`NUM_OF_RO):0];
 	
 	genvar i, j;
 	generate

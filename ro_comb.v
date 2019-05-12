458138f54ebf17efdd247eabd9ec4cf52a8503e2
Bill Kim <munsungk@andrew.cmu.edu>
2019-05-12 15:26:11 -0400

    Made xor binary tree and added registers

--- ro_comb.v
+++ ro_comb.v
@@ -2,23 +2,33 @@
 `include "params.vh"
 
 module ro_comb (
+  input wire enable,
+  input wire clock,
   output wire output_comb);
 
 	wire signals[`NUM_OF_RO-1:0];
-	wire xors[`NUM_OF_RO-2:0];
+	reg [`NUM_OF_RO-1:0] xors[$clog2(`NUM_OF_RO):0];
 	
-	genvar i;
+	genvar i, j;
 	generate
 		for (i = 0; i < `NUM_OF_RO; i = i + 1) begin: ring_oscillators
-			ro #(`NUM_OF_GATES, `GATE_DELAY) ring (signals[i]);
+			ro #(`NUM_OF_GATES, `GATE_DELAY) ring (enable, clock, signals[i]);
 		end
 
-		xor (xors[0], signals[0], signals[1]);
-		for (i = 0; i < `NUM_OF_RO-2; i = i + 1) begin: xor_gates
-			xor (xors[i+1], xors[i], signals[i+2]);
+		for (i = 0; i < $clog2(`NUM_OF_RO); i = i + 1) begin: layers
+			for (j = 0; j < `NUM_OF_RO / 2**i; j = j + 2) begin: xor_gates
+        always @(posedge clock) begin
+          xors[i+1][j/2] <= xors[i][j] ^ xors[i][j + 1];
+        end
+        //xor (xors[i+1][j/2], xors[i][j], xors[i][j + 1]);
+      end
 		end
 	endgenerate
 
-  assign output_comb = xors[`NUM_OF_RO-2];  
+  always @(posedge clock) begin
+    xors[0] <= signals;
+  end
+
+  assign output_comb = xors[$clog2(`NUM_OF_RO)][0];  
 
 endmodule

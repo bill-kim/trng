837dd730714e8cc780dab959060bbe8ab5b4e9ce
Sheng-Hao <shenghah@andrew.cmu.edu>
2019-05-13 01:09:18 -0400

    Added keep true

--- ro.v
+++ ro.v
@@ -1,9 +1,13 @@
 `default_nettype none
 
+(* KEEP_HIERARCHY="TRUE" *)
+
 module ro #(parameter SIZE = 3, DELAY = 2) (
   input wire enable,
   input wire clock,
   output wire signal);
+
+  (* S = "TRUE" *)
 
 	wire out[SIZE-1:0];
   wire init_val;

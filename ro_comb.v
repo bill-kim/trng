26e435256658fac37876d825f3c201296d431281
Bill Kim <munsungk@andrew.cmu.edu>
2019-05-13 01:26:32 -0400

    used reduction operator ^ instead of xor tree

--- ro_comb.v
+++ ro_comb.v
@@ -4,10 +4,10 @@
 module ro_comb (
   input wire enable,
   input wire clock,
-  output wire output_comb);
+  output reg output_comb);
 
-	wire signals[`NUM_OF_RO-1:0];
-	reg [`NUM_OF_RO-1:0] xors[`LOG_NUM_OF_RO:0];
+	wire [`NUM_OF_RO-1:0] signals;
+	//reg [`NUM_OF_RO-1:0] xors[`LOG_NUM_OF_RO:0];
 	integer k;
 
 	genvar i, j;
@@ -15,7 +15,7 @@ module ro_comb (
 		for (i = 0; i < `NUM_OF_RO; i = i + 1) begin: ring_oscillators
 			ro #(`NUM_OF_GATES, `GATE_DELAY) ring (enable, clock, signals[i]);
 		end
-
+/*
 		for (i = 0; i < `LOG_NUM_OF_RO; i = i + 1) begin: layers
 			for (j = 0; j < `NUM_OF_RO / 2**i; j = j + 2) begin: xor_gates
         always @(posedge clock) begin
@@ -24,14 +24,11 @@ module ro_comb (
         //xor (xors[i+1][j/2], xors[i][j], xors[i][j + 1]);
       end
 		end
+*/
 	endgenerate
 
   always @(posedge clock) begin
-    for (k = 0; k < `NUM_OF_RO; k = k + 1) begin
-      xors[0][k] <= signals[k];
-    end
+    output_comb <= ^signals;
   end
-
-  assign output_comb = xors[`LOG_NUM_OF_RO][0];  
 
 endmodule
